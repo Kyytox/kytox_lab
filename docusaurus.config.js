4586894da2c8ecf82485a6ac35300e04ade3f738
Kyytox <kytox.dev@protonmail.com>
2024-02-06 04:21:22 +0100

    change url github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,15 +43,13 @@ const config = {
                     sidebarPath: "./sidebars.js",
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                    editUrl: "https://github.com/Kyytox/kytox_lab",
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                    editUrl: "https://github.com/Kyytox/kytox_lab",
                 },
                 theme: {
                     customCss: "./src/css/custom.css",
