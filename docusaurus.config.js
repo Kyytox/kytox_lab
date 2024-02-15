909a55824ed38dfb81d4bd5b6471f2fa649da002
Kyytox <kytox.dev@protonmail.com>
2024-02-15 20:55:11 +0100

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
     url: "https://github.com",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
+    baseUrl: "/kytox_lab",
     trailingSlash: false,
 
     // GitHub pages deployment config.
